435e39eeb533eb7c2feb64f49af15bd01589f236
Inquient <makoveyfedorich@gmail.com>
2017-10-14 18:04:50 +0300

    Example of drawing graphic

---
+++ Graphic/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Graphic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(62, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(37, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(37, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(37, 73);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "R:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "r:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "x:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(583, 27);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.27536F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.72464F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 143F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(140, 245);
+            this.tableLayoutPanel1.TabIndex = 9;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(735, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеToolStripMenuItem});
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(534, 307);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(189, 45);
+            this.trackBar1.TabIndex = 12;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(595, 291);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Масштаб";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(735, 534);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.SizeChanged += new System.EventHandler(this.trackBar1_Scroll);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Graphic/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphic
+{
+    public partial class Form1 : Form
+    {
+        double R = 0;
+        double r = 0;
+        double arm = 0;
+
+        int scale = 10;
+
+        PointF[] epicycle;
+        PointF[] circle1;
+        PointF[] circle2;
+        PointF[] traectory;
+
+        displayedValues values;
+
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar1.TickFrequency = 10;
+            trackBar1.Minimum = 1;
+            trackBar1.Maximum = 100;
+        }
+
+        private PointF[] getEpiCycle(double R, double r, double arm)
+        {
+            if (arm != 0)
+            {
+                R = (float)((r + R) - arm);
+                r = (float)arm;
+            }
+
+            int size = 360 * (int)r;
+            if(R/r >= 1)
+            {
+                size = 360 * (int)(R / r);
+            }
+
+            PointF[] points = new PointF[0];
+            try
+            {
+                points = new PointF[size];
+            }
+            catch
+            {
+                MessageBox.Show("Ты б ещё 100 нулей наквасил?!");
+            }
+
+            double k = R / r;
+            double a = r * (k + 1);
+            for (int i = 0; i < points.Length; i++)
+            {
+                    double t = i * Math.PI / 180.0;
+
+                    //Эпициклоида
+                    double x = (a * (Math.Cos(t) - Math.Cos((k + 1.0) * t) / (k + 1.0)));
+                    double y = (a * (Math.Sin(t) - Math.Sin((k + 1.0) * t) / (k + 1.0)));
+
+                    //Гипоциклоида
+                    //double x = (a * (Math.Cos(t) + Math.Cos((k - 1.0) * t) / (k - 1.0)));
+                    //double y = (a * (Math.Sin(t) - Math.Sin((k - 1.0) * t) / (k - 1.0)));
+                    points[i] = new PointF((float)x, (float)y);
+                
+            }
+            return points;
+        }
+
+        private PointF[] getCircle(double r, double shift)
+        {
+            PointF[] circle = new PointF[360];
+
+            for (int i = 0; i < circle.Length; i++)
+            {
+                double t = i * Math.PI / 180.0;
+
+                double x = (r * Math.Cos(t)) + shift;
+                double y = r * Math.Sin(t);
+
+                circle[i] = new PointF((float)x, (float)y);
+            }
+            return circle;
+        }
+
+        private void reduceScale()
+        {
+            double min;
+            if (R < r) min = R;
+            else min = r;
+            if (arm < min) min = arm;
+
+            int order = 1;
+            while (min > 10)
+            {
+                min /= 10;
+                order++;
+            }
+
+            R = (R/Math.Pow(10, order)) * scale;
+            r = (r/Math.Pow(10, order)) * scale;
+            arm = (arm/Math.Pow(10, order)) * scale;
+
+            //return Convert.ToString(R) + " " + Convert.ToString(r) + " " + Convert.ToString(arm);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.R = Convert.ToDouble(textBox1.Text);
+                this.r = Convert.ToDouble(textBox2.Text);
+                if (textBox3.Text != "")
+                {
+                    this.arm = Convert.ToDouble(textBox3.Text);
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Неверный формат ввода!");
+            }
+
+            values = new displayedValues(R, r, arm);
+
+            reduceScale();
+            
+            epicycle = getEpiCycle(this.R, this.r, this.arm);
+            circle1 = getCircle(this.R, 0);
+            circle2 = getCircle(this.r, this.R + this.r);
+            traectory = getCircle(this.R + this.r, 0);
+
+            //MessageBox.Show(Convert.ToString(reduceScale()));
+
+            pictureBox1.Refresh();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+            "R - радиус стационарной окружности\n r - радиус катящейся окружности\n x - расстояние до точки Х",
+            "Инструкция",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Information);
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics grafen = e.Graphics;
+
+            Pen pendefault = new Pen(Color.Gray, 1);
+            Pen penCrcle = new Pen(Color.DarkRed, 1);
+            Pen penTraectory = new Pen(Color.Yellow, 1);
+            Pen penEpicycle = new Pen(Color.DeepSkyBlue, 1);
+
+            grafen.DrawLine(pendefault, new Point(0, pictureBox1.Height / 2), new Point(pictureBox1.Height, pictureBox1.Height / 2));
+            grafen.DrawLine(pendefault, new Point(pictureBox1.Width / 2, 0), new Point(pictureBox1.Width / 2, pictureBox1.Width));
+
+            try
+            {
+                for (int i = 0; i < circle1.Length; i++)
+                {
+                    grafen.DrawRectangle(penCrcle, (float)(circle1[i].X + pictureBox1.Height / 2), (float)(-1 * (circle1[i].Y - pictureBox1.Width / 2)), 1, 1);
+                    grafen.DrawRectangle(penCrcle, (float)(circle2[i].X + pictureBox1.Height / 2), (float)(-1 * (circle2[i].Y - pictureBox1.Width / 2)), 1, 1);
+                    grafen.DrawRectangle(penTraectory, (float)(traectory[i].X + pictureBox1.Height / 2), (float)(-1 * (traectory[i].Y - pictureBox1.Width / 2)), 1, 1);
+                }
+                for (int i = 0; i < epicycle.Length; i++)
+                {
+                    grafen.DrawRectangle(penEpicycle, (float)(epicycle[i].X + pictureBox1.Height / 2), (float)(-1 * (epicycle[i].Y - pictureBox1.Width / 2)), 1, 1);
+
+                }
+                grafen.DrawString(values.radiusR, new Font(FontFamily.GenericSansSerif, 10, FontStyle.Regular),
+                    new SolidBrush(Color.Black), (float)this.R + (pictureBox1.Height / 2), pictureBox1.Width / 2);
+                grafen.DrawString(values.radiusr, new Font(FontFamily.GenericSansSerif, 10, FontStyle.Regular),
+                    new SolidBrush(Color.Black), (float)this.R + 2*(float)this.r + pictureBox1.Height / 2, (pictureBox1.Width / 2)-10);
+            }
+            catch
+            {
+                grafen.DrawRectangle(penCrcle, 0 + pictureBox1.Height / 2, (-1 * (0 - pictureBox1.Width / 2)), 1, 1);
+            }
+        }
+
+        struct displayedValues
+        {
+            public String radiusR;
+            public String radiusr;
+
+            public displayedValues(double R, double r, double arm)
+            {
+                radiusR = Convert.ToString(R);
+                radiusr = Convert.ToString(r + R);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            scale = trackBar1.Value;
+            label4.Text = "Масштаб - " + Convert.ToString(scale);
+
+            reduceScale();
+            epicycle = getEpiCycle(this.R, this.r, this.arm);
+            circle1 = getCircle(this.R, 0);
+            circle2 = getCircle(this.r, this.R + this.r);
+            traectory = getCircle(this.R + this.r, 0);
+            pictureBox1.Refresh();
+        }
+    }
+}
